bba6564c3f838ed1b995f87bc04d0280cfef5756
Yauheniya Kavalchuk <y.kavalchuk@godeltech.com>
2025-07-10 17:46:43 +0300

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Пиццерия "У Лоренцио"</h1>
+      <p>Я Лоренцио и я хочу чтоб ты купил мою пиццу!!!!</p>
+    </div>
+  );
+}
