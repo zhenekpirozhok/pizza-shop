017541d23673368d8c6e9b1e802ceec4637a1675
Yauheniya Kavalchuk <y.kavalchuk@godeltech.com>
2025-07-15 16:07:40 +0300

    add styles to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div>
-      <h1>Пиццерия "У Лоренцио"</h1>
-      <p>Я Лоренцио и я хочу чтоб ты купил мою пиццу!!!!</p>
-    </div>
+    <main className="min-h-screen flex flex-col items-start justify-center bg-[url(/images/pizza-bg.png)] bg-center before:absolute before:inset-0 before:bg-gradient-to-r before:from-neutral-900/80 before:to-neutral-900/10">
+      <div className="relative z-10 flex flex-col items-start justify-center sm:px-30 px-10">
+      <h1 className="text-4xl font-bold sm:text-7xl text-white">All the best<br/> things are round</h1>
+      <p className="text-neutral-200 py-5">Our pizzas are made by slaves... Oh, I really said that? I meant by the best italian chefs, yeah.</p>
+      <button className="bg-red-800 hover:bg-red-900 text-white shadow-md py-4 px-6 rounded-full cursor-pointer text-md uppercase">Order now</button>
+      </div>
+    </main>
   );
 }
