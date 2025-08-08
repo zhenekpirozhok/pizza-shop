747ed860ed85a172680c670f66be9472376cf059
Yauheniya Kavalchuk <y.kavalchuk@godeltech.com>
2025-08-08 16:36:05 +0300

    add prettier and static rendering for menu

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,38 +1,40 @@
-'use client';
+import { Pizza } from '../types/pizza.type';
+import { PizzaCard } from '../ui/menu/pizza-card';
 
-import { useEffect, useState } from "react";
-import { Pizza } from "../types/pizza.type";
-import { PizzaCard } from "../ui/menu/pizza-card";
+async function fetchPizzas() {
+  try {
+    const response = await fetch('http://localhost:3000/api/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: `{ pizzas { id name price image } }`,
+      }),
+    });
+    const data = await response.json();
+    return data.data.pizzas;
+  } catch (error) {
+    console.error('Error fetching pizzas:', error);
+  }
+}
 
-export default function Page() {
-    const [pizzas, setPizzas] = useState<Pizza[]>([]);
+export default async function Page() {
+  const pizzas = await fetchPizzas();
 
-    useEffect(() => {
-        async function fetchPizzas() {
-            try {
-                const response = await fetch('/api/graphql', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        query: `{ pizzas { id name price image } }`
-                    })
-                });
-                const data = await response.json();
-                setPizzas(data.data.pizzas);
-            } catch (error) {
-                console.error('Error fetching pizzas:', error);
-            }
-        }
-
-        fetchPizzas();
-    }, []);
-
-    return <div className="flex flex-col items-center gap-5 py-10 px-5">
-        <p className="text-red-700 font-bold md:text-lg text-sm uppercase">Our Selection</p>
-        <h1 className="md:text-4xl text-2xl font-bold md:w-1/2 w-full text-center">A Menu That Will Always Capture Your Heart</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 py-10">
-            {pizzas.map((pizza) => <PizzaCard key={pizza.id} pizza={pizza} />)}</div>           
-    </div>;
-};
+  return (
+    <div className="flex flex-col items-center gap-5 py-10 px-5">
+      <p className="text-red-700 font-bold md:text-lg text-sm uppercase">
+        Our Selection
+      </p>
+      <h1 className="md:text-4xl text-2xl font-bold md:w-1/2 w-full text-center">
+        A Menu That Will Always Capture Your Heart
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 py-10">
+        {pizzas.map((pizza: Pizza) => (
+          <PizzaCard key={pizza.id} pizza={pizza} />
+        ))}
+      </div>
+    </div>
+  );
+}
